e29f781b8adb8b3918b373f52c70f3410bff9bbd
suay-selay <suay_999@hotmail.com>
2021-11-18 16:04:58 +0100

    added annotations

--- src/main/java/se/iths/entity/Student.java
+++ src/main/java/se/iths/entity/Student.java
@@ -1,17 +1,33 @@
 package se.iths.entity;
 
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
+
+@Entity
 public class Student {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    // Constraint, förhindrar att ett namn med mindre än 2 bokstäver anges
+    @NotEmpty
+    @Size(min = 2)
     private String firstName;
+    @NotEmpty
+    @Size(min = 2)
     private String lastName;
+    @NotEmpty
+    @Email(message = "Email should be valid")
     private String email;
+
     private String phoneNumber;
 
     public Long getId() {
