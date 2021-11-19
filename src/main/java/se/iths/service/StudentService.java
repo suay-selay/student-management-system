06ab5b8ceac154706a868c19952855c7ef05de0a
suay-selay <suay_999@hotmail.com>
2021-11-19 12:08:44 +0100

    added methods to service class

--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,39 @@
 package se.iths.service;
 
+import se.iths.entity.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
 
 public class StudentService {
+    @PersistenceContext
+    EntityManager entityManager;
 
+    public Student createStudent(Student student){
+        entityManager.persist(student);
+        return student;
+    }
+
+    public Student updateStudent(Student student){
+        entityManager.merge(student);
+        return student;
+    }
+
+    public Student findStudentById(Long id){
+        return entityManager.find(Student.class, id);
+    }
+
+    public List<Student> getAllStudents(){
+        return entityManager.createQuery("SELECT s from Student s", Student.class ).getResultList();
+    }
+
+    public void deleteStudent(Long id) {
+        Student foundStudent = entityManager.find(Student.class, id);
+        entityManager.remove(foundStudent);
+    }
 
 }
