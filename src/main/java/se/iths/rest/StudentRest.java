341ea3e0cbd892a70729b8ae1111af80d2bd2f27
suay-selay <suay_999@hotmail.com>
2021-11-19 12:10:12 +0100

    Added methods to service class

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,15 +1,6 @@
 package se.iths.rest;
 
-import se.iths.service.StudentService;
-
-import javax.inject.Inject;
-import javax.ws.rs.Path;
-
-@Path("student")
 public class StudentRest {
-
-    @Inject
-    StudentService studentService;
 
 
 }
