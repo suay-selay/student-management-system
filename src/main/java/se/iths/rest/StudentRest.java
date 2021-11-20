b3e6d44e9e3d02a2569ede7daaa032f13768a88e
suay-selay <suay_999@hotmail.com>
2021-11-20 20:33:44 +0100

    delete method etc

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,9 +1,62 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("student")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    @Inject
+    StudentService studentService;
 
+    @Path("new")
+    @POST
+    public Response createStudent(Student student){
+        studentService.createStudent(student);
+        return Response.ok(student).build();
+    }
 
+    @Path("update")
+    @PUT
+    public Response updateStudent(Student student){
+        studentService.updateStudent(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Response getStudent(@PathParam("id") Long id) {
+        Student foundStudent = studentService.findStudentById(id);
+        if (foundStudent == null) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("Student with ID " + id + " was not found in database.").type(MediaType.TEXT_PLAIN_TYPE).build());
+        }
+        return Response.ok(foundStudent).build();
+    }
+
+    @Path("getall")
+    @GET
+    public Response getAllStudents() {
+        List<Student> foundStudent = studentService.getAllStudents();
+        return Response.ok(foundStudent).build();
+    }
+
+    @Path("{id}")
+    @DELETE
+    public Response deleteStudent(@PathParam("id") Long id) {
+        //Student studentToDelete = studentService.findStudentById(id);
+        //studentService.deleteStudent(studentToDelete);
+        studentService.deleteStudent(id);
+        return Response.ok().build();
+    }
 
 
 
