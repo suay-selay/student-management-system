5ce09898fe697f46c8dda298e4d04cfabef83a8c
suay-selay <suay_999@hotmail.com>
2021-12-02 14:16:07 +0100

    some methods for service and rest classes

--- src/main/java/se/iths/service/SubjectService.java
+++ src/main/java/se/iths/service/SubjectService.java
@@ -1,14 +1,29 @@
 package se.iths.service;
 
 
+import se.iths.entity.Subject;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 public class SubjectService {
 
     @PersistenceContext
     EntityManager entityManager;
+
+    public void createSubject(Subject subject){
+        entityManager.persist(subject);
+    }
+
+    public Subject findSubjectById(Long id){
+        return entityManager.find(Subject.class, id);
+    }
+
+    public List<Subject> getAllSubjects(){
+        return entityManager.createQuery("SELECT s from Subject s", Subject.class ).getResultList();
+    }
 
 }
--- src/main/java/se/iths/service/TeacherService.java
+++ src/main/java/se/iths/service/TeacherService.java
@@ -1,14 +1,29 @@
 package se.iths.service;
 
 
+import se.iths.entity.Teacher;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 public class TeacherService {
 
     @PersistenceContext
     EntityManager entityManager;
+
+    public void createTeacher(Teacher teacher){
+        entityManager.persist(teacher);
+    }
+
+    public Teacher findTeacherById(Long id){
+        return entityManager.find(Teacher.class, id);
+    }
+
+    public List<Teacher> getAllTeachers(){
+        return entityManager.createQuery("SELECT t from Teacher t", Teacher.class ).getResultList();
+    }
 
 }
